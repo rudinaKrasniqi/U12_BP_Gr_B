078b0cf025eb085d7d2bed23c353aa0e51711684
Rudina Krasniqi <rudine.krasniqi@riinvest.net>
2020-05-21 15:02:25 +0200

    Init

---
+++ U12_BP_Gr_B/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace U12_BP_Gr_B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTeksti = new System.Windows.Forms.Label();
+            this.lblKerko = new System.Windows.Forms.Label();
+            this.txtTeksti = new System.Windows.Forms.TextBox();
+            this.txtKerko = new System.Windows.Forms.TextBox();
+            this.btnNrShkronja = new System.Windows.Forms.Button();
+            this.btnNrNumrave = new System.Windows.Forms.Button();
+            this.btnNrShkronjaTeMedha = new System.Windows.Forms.Button();
+            this.btnNrShkronjaTeVogla = new System.Windows.Forms.Button();
+            this.btnCapitalizeWord = new System.Windows.Forms.Button();
+            this.btnKerko = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTeksti
+            // 
+            this.lblTeksti.AutoSize = true;
+            this.lblTeksti.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTeksti.Location = new System.Drawing.Point(33, 13);
+            this.lblTeksti.Name = "lblTeksti";
+            this.lblTeksti.Size = new System.Drawing.Size(146, 24);
+            this.lblTeksti.TabIndex = 0;
+            this.lblTeksti.Text = "Shkruaj nje tekst";
+            // 
+            // lblKerko
+            // 
+            this.lblKerko.AutoSize = true;
+            this.lblKerko.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKerko.Location = new System.Drawing.Point(432, 345);
+            this.lblKerko.Name = "lblKerko";
+            this.lblKerko.Size = new System.Drawing.Size(59, 24);
+            this.lblKerko.TabIndex = 0;
+            this.lblKerko.Text = "Kerko";
+            // 
+            // txtTeksti
+            // 
+            this.txtTeksti.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTeksti.Location = new System.Drawing.Point(36, 47);
+            this.txtTeksti.Multiline = true;
+            this.txtTeksti.Name = "txtTeksti";
+            this.txtTeksti.Size = new System.Drawing.Size(356, 370);
+            this.txtTeksti.TabIndex = 1;
+            // 
+            // txtKerko
+            // 
+            this.txtKerko.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKerko.Location = new System.Drawing.Point(436, 372);
+            this.txtKerko.Name = "txtKerko";
+            this.txtKerko.Size = new System.Drawing.Size(188, 29);
+            this.txtKerko.TabIndex = 1;
+            // 
+            // btnNrShkronja
+            // 
+            this.btnNrShkronja.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNrShkronja.Location = new System.Drawing.Point(436, 47);
+            this.btnNrShkronja.Name = "btnNrShkronja";
+            this.btnNrShkronja.Size = new System.Drawing.Size(333, 39);
+            this.btnNrShkronja.TabIndex = 2;
+            this.btnNrShkronja.Text = "Numri i shkronjave";
+            this.btnNrShkronja.UseVisualStyleBackColor = true;
+            this.btnNrShkronja.Click += new System.EventHandler(this.btnNrShkronja_Click);
+            // 
+            // btnNrNumrave
+            // 
+            this.btnNrNumrave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNrNumrave.Location = new System.Drawing.Point(436, 102);
+            this.btnNrNumrave.Name = "btnNrNumrave";
+            this.btnNrNumrave.Size = new System.Drawing.Size(333, 39);
+            this.btnNrNumrave.TabIndex = 2;
+            this.btnNrNumrave.Text = "Numri i numrave";
+            this.btnNrNumrave.UseVisualStyleBackColor = true;
+            this.btnNrNumrave.Click += new System.EventHandler(this.btnNrNumrave_Click);
+            // 
+            // btnNrShkronjaTeMedha
+            // 
+            this.btnNrShkronjaTeMedha.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNrShkronjaTeMedha.Location = new System.Drawing.Point(436, 156);
+            this.btnNrShkronjaTeMedha.Name = "btnNrShkronjaTeMedha";
+            this.btnNrShkronjaTeMedha.Size = new System.Drawing.Size(333, 39);
+            this.btnNrShkronjaTeMedha.TabIndex = 2;
+            this.btnNrShkronjaTeMedha.Text = "Numri i shkronjave te medha";
+            this.btnNrShkronjaTeMedha.UseVisualStyleBackColor = true;
+            this.btnNrShkronjaTeMedha.Click += new System.EventHandler(this.btnNrShkronjaTeMedha_Click);
+            // 
+            // btnNrShkronjaTeVogla
+            // 
+            this.btnNrShkronjaTeVogla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNrShkronjaTeVogla.Location = new System.Drawing.Point(436, 214);
+            this.btnNrShkronjaTeVogla.Name = "btnNrShkronjaTeVogla";
+            this.btnNrShkronjaTeVogla.Size = new System.Drawing.Size(333, 39);
+            this.btnNrShkronjaTeVogla.TabIndex = 2;
+            this.btnNrShkronjaTeVogla.Text = "Numri i shkronjave te vogla";
+            this.btnNrShkronjaTeVogla.UseVisualStyleBackColor = true;
+            this.btnNrShkronjaTeVogla.Click += new System.EventHandler(this.btnNrShkronjaTeVogla_Click);
+            // 
+            // btnCapitalizeWord
+            // 
+            this.btnCapitalizeWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCapitalizeWord.Location = new System.Drawing.Point(436, 277);
+            this.btnCapitalizeWord.Name = "btnCapitalizeWord";
+            this.btnCapitalizeWord.Size = new System.Drawing.Size(333, 39);
+            this.btnCapitalizeWord.TabIndex = 2;
+            this.btnCapitalizeWord.Text = "Capitalize Word";
+            this.btnCapitalizeWord.UseVisualStyleBackColor = true;
+            this.btnCapitalizeWord.Click += new System.EventHandler(this.btnCapitalizeWord_Click);
+            // 
+            // btnKerko
+            // 
+            this.btnKerko.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKerko.Location = new System.Drawing.Point(643, 358);
+            this.btnKerko.Name = "btnKerko";
+            this.btnKerko.Size = new System.Drawing.Size(126, 43);
+            this.btnKerko.TabIndex = 2;
+            this.btnKerko.Text = "Kerko";
+            this.btnKerko.UseVisualStyleBackColor = true;
+            this.btnKerko.Click += new System.EventHandler(this.btnKerko_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKerko);
+            this.Controls.Add(this.btnCapitalizeWord);
+            this.Controls.Add(this.btnNrShkronjaTeVogla);
+            this.Controls.Add(this.btnNrShkronjaTeMedha);
+            this.Controls.Add(this.btnNrNumrave);
+            this.Controls.Add(this.btnNrShkronja);
+            this.Controls.Add(this.txtKerko);
+            this.Controls.Add(this.txtTeksti);
+            this.Controls.Add(this.lblKerko);
+            this.Controls.Add(this.lblTeksti);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTeksti;
+        private System.Windows.Forms.Label lblKerko;
+        private System.Windows.Forms.TextBox txtTeksti;
+        private System.Windows.Forms.TextBox txtKerko;
+        private System.Windows.Forms.Button btnNrShkronja;
+        private System.Windows.Forms.Button btnNrNumrave;
+        private System.Windows.Forms.Button btnNrShkronjaTeMedha;
+        private System.Windows.Forms.Button btnNrShkronjaTeVogla;
+        private System.Windows.Forms.Button btnCapitalizeWord;
+        private System.Windows.Forms.Button btnKerko;
+    }
+}
+
---
+++ U12_BP_Gr_B/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U12_BP_Gr_B
+{
+    // Char 
+    // IsLetter()  => shkronja
+    // IsDigit()  => numer
+    // IsUpper() => shkronje e madhe
+    // IsLower() => shkonje e vogel 
+
+    public partial class Form1 : Form
+    {
+        string tekstiNgaPerdoruesi = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Eventet e butonave
+        private void btnNrShkronja_Click(object sender, EventArgs e)
+        {
+            tekstiNgaPerdoruesi = txtTeksti.Text;
+
+            //Metoda 1
+            MessageBox.Show("Numri i shkronjave: " +
+                NumriIShkronja(tekstiNgaPerdoruesi));
+
+            //Metoda 2
+            //int nrShkronjave = NumriIShkronja(tekstiNgaPerdoruesi);
+            //MessageBox.Show("Numri i shkronjave: " + nrShkronjave);
+        }
+
+        private void btnNrNumrave_Click(object sender, EventArgs e)
+        {
+            tekstiNgaPerdoruesi = txtTeksti.Text;
+            MessageBox.Show("Numri i numrave: " + 
+                NumriINumrave(tekstiNgaPerdoruesi));
+        }
+
+        private void btnNrShkronjaTeMedha_Click(object sender, EventArgs e)
+        {
+            tekstiNgaPerdoruesi = txtTeksti.Text;
+            MessageBox.Show("Numri i shkronjave te medha: " +
+                     NumriIShkronjaveTeMedha(tekstiNgaPerdoruesi));
+        }
+
+        private void btnNrShkronjaTeVogla_Click(object sender, EventArgs e)
+        {
+            tekstiNgaPerdoruesi = txtTeksti.Text;
+            int numriIShkronjave;
+            NumriIShkronjaveTeVogla(tekstiNgaPerdoruesi, out numriIShkronjave);
+            MessageBox.Show("Numri i shkronjave te vogla: " + numriIShkronjave);
+        }
+
+        private void btnCapitalizeWord_Click(object sender, EventArgs e)
+        {
+            tekstiNgaPerdoruesi = txtTeksti.Text;
+            txtTeksti.Text = CapitalizeFirstWord(tekstiNgaPerdoruesi);
+        }
+
+        private void btnKerko_Click(object sender, EventArgs e)
+        {
+            tekstiNgaPerdoruesi = txtTeksti.Text;
+            string fjala = txtKerko.Text;
+
+            if (DoesAWordExistInAText(tekstiNgaPerdoruesi, fjala))
+            {
+                MessageBox.Show("U gjet fjala!");
+            }
+            else
+            {
+                MessageBox.Show("Fjala nuk u gjet");
+            }
+        }
+
+        #endregion
+
+        #region Metodat
+
+        private int NumriIShkronja(string teksti)
+        {
+            int count = 0;
+            for(int i=0; i< teksti.Length;i++)
+            {
+                if (Char.IsLetter(teksti[i]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private int NumriINumrave(string teksti)
+        {
+            int count = 0;
+            for (int i = 0; i < teksti.Length; i++)
+            {
+                if(Char.IsDigit(teksti[i]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private int NumriIShkronjaveTeMedha(string teksti)
+        {
+            int count = 0;
+            for (int i = 0; i < teksti.Length; i++)
+            {
+                if (Char.IsUpper(teksti[i]))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private void NumriIShkronjaveTeVogla(string teksti, 
+            out int nrShkronja)
+        {
+            nrShkronja = 0;
+            for (int i = 0; i < teksti.Length; i++)
+            {
+                if(Char.IsLower(teksti[i]))
+                {
+                    nrShkronja++;
+                }
+            }
+        }
+
+        // bazat e programimit
+        // b
+        // azat e programimit
+        private string CapitalizeFirstWord(string text)
+        {
+            string firstLetter = text[0].ToString();
+            string otherPartOfText = text.Substring(1, text.Length-1);
+
+            return firstLetter.ToUpper() + otherPartOfText.ToLower();
+        }
+
+        private bool DoesAWordExistInAText(string  text, string word)
+        {
+            string pattern = @"" + word;
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(text);
+            if (match.Success)
+            {
+                return true;
+            }
+            return false;
+        }
+        #endregion
+    }
+}
